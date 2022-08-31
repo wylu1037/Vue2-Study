3249f64993f86fc20a05c9eb7e62c8aad237b7bd
wylu <wylu@zkjg.com>
2022-08-31 12:46:50 +0800

    chore: add delete chain button func

--- src/pages/Chain/chainList.vue
+++ src/pages/Chain/chainList.vue
@@ -1,73 +1,114 @@
 <template>
-  <el-main>
-    <el-button
-      type="primary"
-      style="margin-right: -1190px; margin-bottom: 20px; position: relative"
-      @click="getAllChainList"
-      >查询</el-button
-    >
-    <el-table border :data="chainList">
-      <el-table-column type="index" label="序号" width="200"></el-table-column>
-      <el-table-column prop="Name" label="名称"> </el-table-column>
-      <el-table-column
-        prop="ChainType"
-        label="链类型"
-        filter-placement="bottom-end"
-      >
-        <template slot-scope="scope">
-          <el-tag
-            :type="scope.row.ChainType === 0 ? 'primary' : 'success'"
-            disable-transitions
-            >{{ matchChainTypeName(scope.row.ChainType) }}</el-tag
-          >
-        </template>
-      </el-table-column>
-      <el-table-column prop="BlockChainId" label="BlockChainID">
-      </el-table-column>
-      <el-table-column fixed="right" label="操作" width="120px" align="center">
-        <template slot-scope="scope">
-          <el-button
-            @click="queryChainInfo(scope.row.ID)"
-            icon="el-icon-search"
-            circle
-            size="small"
-          ></el-button>
-          <el-button
-            type="danger"
-            icon="el-icon-delete"
-            circle
-            size="small"
-          ></el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-  </el-main>
+  <div class="chain-list-main">
+    <el-main>
+      <div>
+        <el-button
+          class="chain-list-query-btn"
+          type="primary"
+          @click="getAllChainList"
+          >查询</el-button
+        >
+      </div>
+      <el-table border :data="chainList">
+        <el-table-column
+          type="index"
+          label="序号"
+          width="200"
+        ></el-table-column>
+        <el-table-column prop="Name" label="名称"> </el-table-column>
+        <el-table-column
+          prop="ChainType"
+          label="链类型"
+          filter-placement="bottom-end"
+        >
+          <template slot-scope="scope">
+            <el-tag
+              :type="scope.row.ChainType === 0 ? 'primary' : 'success'"
+              disable-transitions
+              >{{ matchChainTypeName(scope.row.ChainType) }}</el-tag
+            >
+          </template>
+        </el-table-column>
+        <el-table-column prop="BlockChainId" label="BlockChainID">
+        </el-table-column>
+        <el-table-column
+          fixed="right"
+          label="操作"
+          width="120px"
+          align="center"
+        >
+          <template slot-scope="scope">
+            <el-button
+              @click="queryChainInfo(scope.row.ID)"
+              icon="el-icon-search"
+              circle
+              size="small"
+            ></el-button>
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              circle
+              size="small"
+              @click="deleteChainById(scope.row.ID)"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-main>
+  </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import { mapState } from "vuex";
 
 export default {
-    name: 'ChainList',
+  name: "ChainList",
 
-    methods: {
-        matchChainTypeName(type) {
-            return type == 0 ? '晶格链' : '超级账本'
-        },
-
-        getAllChainList() {
-            this.$store.dispatch('latticeModule/getAllChainList')
-        },
-
-        queryChainInfo(chainId) {
-            alert('链ID：' + chainId)
-        },
+  methods: {
+    matchChainTypeName(type) {
+      return type == 0 ? "晶格链" : "超级账本";
     },
 
-    computed: {
-        ...mapState('latticeModule', ['chainList'])
-    }
+    getAllChainList() {
+      this.$store.dispatch("latticeModule/getAllChainList");
+    },
+
+    queryChainInfo(chainId) {
+      alert("链ID：" + chainId);
+    },
+
+    deleteChainById(chainId) {
+      this.$confirm("此操作将永久删除链，是否继续？", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          // 调用接口删除链
+          console.log("@chainId: " + chainId);
+          this.$message({ type: "success", message: "删除成功！" });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除！",
+          });
+        });
+    },
+  },
+
+  computed: {
+    ...mapState("latticeModule", ["chainList"]),
+  },
 };
 </script>
 
-<style></style>
+<style lang="css" scoped>
+.chain-list-main {
+  width: 100vw;
+}
+
+.chain-list-query-btn {
+  margin-left: auto;
+}
+</style>
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,121 +1,127 @@
 <template>
-    <div class="home-root">
-        <div>
-            <Nav></Nav>
-        </div>
-        <div>
-            <el-container style="height: 757px">
-                <el-aside
-                    width="200px"
-                    height="757px"
-                    style="background-color: rgb(238, 241, 246)"
-                >
-                    <el-menu :default-openeds="['1', '3']">
-                        <el-submenu index="1">
-                            <template slot="title"
-                                ><i class="el-icon-message"></i>导航一</template
-                            >
-                            <el-menu-item-group>
-                                <template slot="title">分组一</template>
-                                <el-menu-item index="1-1"><router-link style="text-decoration:none" to="/home/chain/list">区块链</router-link></el-menu-item>
-                                <el-menu-item index="1-2"><router-link style="text-decoration:none" to="/home/node/list">节点信息</router-link></el-menu-item>
-                            </el-menu-item-group>
-                            <el-menu-item-group title="分组2">
-                                <el-menu-item index="1-3">选项3</el-menu-item>
-                            </el-menu-item-group>
-                            <el-submenu index="1-4">
-                                <template slot="title">选项4</template>
-                                <el-menu-item index="1-4-1"
-                                    >选项4-1</el-menu-item
-                                >
-                            </el-submenu>
-                        </el-submenu>
-                        <el-submenu index="2">
-                            <template slot="title"
-                                ><i class="el-icon-menu"></i>导航二</template
-                            >
-                            <el-menu-item-group>
-                                <template slot="title">分组一</template>
-                                <el-menu-item index="2-1">选项1</el-menu-item>
-                                <el-menu-item index="2-2">选项2</el-menu-item>
-                            </el-menu-item-group>
-                            <el-menu-item-group title="分组2">
-                                <el-menu-item index="2-3">选项3</el-menu-item>
-                            </el-menu-item-group>
-                            <el-submenu index="2-4">
-                                <template slot="title">选项4</template>
-                                <el-menu-item index="2-4-1"
-                                    >选项4-1</el-menu-item
-                                >
-                            </el-submenu>
-                        </el-submenu>
-                        <el-submenu index="3">
-                            <template slot="title"
-                                ><i class="el-icon-setting"></i>导航三</template
-                            >
-                            <el-menu-item-group>
-                                <template slot="title">分组一</template>
-                                <el-menu-item index="3-1">选项1</el-menu-item>
-                                <el-menu-item index="3-2">选项2</el-menu-item>
-                            </el-menu-item-group>
-                            <el-menu-item-group title="分组2">
-                                <el-menu-item index="3-3">选项3</el-menu-item>
-                            </el-menu-item-group>
-                            <el-submenu index="3-4">
-                                <template slot="title">选项4</template>
-                                <el-menu-item index="3-4-1"
-                                    >选项4-1</el-menu-item
-                                >
-                            </el-submenu>
-                        </el-submenu>
-                    </el-menu>
-                </el-aside>
-
-                <el-container>
-                    <router-view></router-view>
-                </el-container>
-            </el-container>
-        </div>
+  <div class="home-root">
+    <div>
+      <Nav></Nav>
     </div>
+    <div>
+      <el-container style="height: 757px">
+        <el-aside
+          width="200px"
+          height="757px"
+          style="background-color: rgb(238, 241, 246)"
+        >
+          <el-menu :default-openeds="['1', '3']">
+            <el-submenu index="1">
+              <template slot="title"
+                ><i class="el-icon-message"></i>导航一</template
+              >
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1"
+                  ><router-link
+                    style="text-decoration: none"
+                    to="/home/chain/list"
+                    >区块链</router-link
+                  ></el-menu-item
+                >
+                <el-menu-item index="1-2"
+                  ><router-link
+                    style="text-decoration: none"
+                    to="/home/node/list"
+                    >节点信息</router-link
+                  ></el-menu-item
+                >
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title"
+                ><i class="el-icon-menu"></i>导航二</template
+              >
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="2-1">选项1</el-menu-item>
+                <el-menu-item index="2-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="2-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="2-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title"
+                ><i class="el-icon-setting"></i>导航三</template
+              >
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="3-1">选项1</el-menu-item>
+                <el-menu-item index="3-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="3-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="3-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+
+        <el-container style="width: 100vw">
+          <router-view></router-view>
+        </el-container>
+      </el-container>
+    </div>
+  </div>
 </template>
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 
 export default {
-    name: 'Home',
+  name: "Home",
 
-    methods: {
-        matchChainTypeName(type) {
-            return type == 0 ? '晶格链' : '超级账本'
-        },
-
-        getAllChainList() {
-            this.$store.dispatch('latticeModule/getAllChainList')
-        },
-
-        queryChainInfo(chainId) {
-            console.log(chainId)
-        },
+  methods: {
+    matchChainTypeName(type) {
+      return type == 0 ? "晶格链" : "超级账本";
     },
 
-    computed: {
-        ...mapState('latticeModule', ['chainList']),
+    getAllChainList() {
+      this.$store.dispatch("latticeModule/getAllChainList");
     },
-}
+
+    queryChainInfo(chainId) {
+      console.log(chainId);
+    },
+  },
+
+  computed: {
+    ...mapState("latticeModule", ["chainList"]),
+  },
+};
 </script>
 
 <style lang="css">
 .el-header {
-    background-color: #b3c0d1;
-    color: #333;
-    line-height: 60px;
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
 }
 
 .el-aside {
-    color: #333;
+  color: #333;
 }
 
 .home-root {
-    height: 100vh;
+  height: 100vh;
 }
 </style>
