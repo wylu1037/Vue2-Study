4adbc0a0174189d149738a349fffd715932692e8
卢文扬 <2052987369@qq.com>
2022-09-04 00:51:09 +0800

    chore: 调整注册页面样式

--- src/pages/Register/index.vue
+++ src/pages/Register/index.vue
@@ -1,57 +1,64 @@
 <template>
-    <div class="registerContainer">
-        <div>
-            <h1>欢迎注册</h1>
-        </div>
+  <!-- 样式调整部分 -->
+  <div class="register-box">
+    <!-- 注册部分 -->
+    <div class="register-content">
+      <div style="text-align: center; margin-bottom: 10%">
+        <h1>欢迎注册</h1>
+      </div>
 
-        <el-form>
-            <el-form-item>
-                <el-input placeholder="姓名"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-input placeholder="邮箱"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-input placeholder="验证码"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-input placeholder="请设置密码"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-input placeholder="请确认密码"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-input placeholder="地址"></el-input>
-            </el-form-item>
+      <el-form>
+        <el-form-item>
+          <el-input placeholder="姓名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="邮箱"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="验证码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请设置密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请确认密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="地址"></el-input>
+        </el-form-item>
 
-            <el-form-item>
-                <el-button type="primary">同意条款并注册</el-button>
-            </el-form-item>
-        </el-form>
-        <div class="tip">
-            已有账号？<a
-                href="login"
-                style="text-decoration-line: none; font-weight: bold"
-                >登录已有账号</a
-            >
-        </div>
+        <el-form-item style="text-align: center">
+          <el-button type="primary">同意条款并注册</el-button>
+        </el-form-item>
+      </el-form>
+      <div style="text-align: center" class="tip">
+        已有账号？<a
+          href="login"
+          style="text-decoration-line: none; font-weight: bold"
+          >登录已有账号</a
+        >
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Register',
-}
+  name: "Register",
+};
 </script>
 
 <style lang="css" scoped>
-.registerContainer {
-    border-radius: 20px;
-    width: 500px;
-    margin: auto;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+.register-box {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+}
+
+.register-content {
+  display: block;
+  border-radius: 20px;
+  width: 500px;
 }
 </style>
