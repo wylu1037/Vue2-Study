b632e542a5d62009a6d660cd4bc8b573eb1d30de
卢文扬 <2052987369@qq.com>
2022-09-03 23:28:16 +0800

    chore: 删除多余的mutations

--- src/store/nav/index.js
+++ src/store/nav/index.js
@@ -22,36 +22,6 @@ const mutations = {
   SETTABS(state, tabs) {
     state.tabs = tabs
   },
-
-  ADDTAB(state, tab, tabValue) {
-    if (JSON.stringify(state.tabs).indexOf(JSON.stringify(tab)) === -1) {
-      state.tabs.push(tab);
-    }
-    state.tabValue = tabValue;
-    state.closable = state.tabs.length !== 1;
-  },
-
-  EDITTAB(state, targetName) {
-    let pushFlag = targetName === state.tabValue;
-      let tabs = state.tabs;
-      let activeName = state.tabValue;
-      if (activeName === targetName) {
-        tabs.forEach((tab, index) => {
-          if (tab.name === targetName) {
-            let nextTab = tabs[index + 1] || tabs[index - 1];
-            if (nextTab) {
-              activeName = nextTab.name;
-            }
-          }
-        });
-      }
-      state.tabValue = activeName;
-      if (pushFlag) {
-        // this.$router.push(state.tabRoute[activeName]);
-      }
-      state.tabs = tabs.filter((tab) => tab.name !== targetName);
-      state.closable = state.tabs.length !== 1;
-  },
 };
 
 const getters = {};
